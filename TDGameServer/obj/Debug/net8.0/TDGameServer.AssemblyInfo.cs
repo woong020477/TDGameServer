a70c19f52d4c8ab230b2e6b79aeb7b557a83fac7
KangJiwoong <woong020477@gmail.com>
2025-09-03 16:26:43 +0900

    Update
    서버 DB 백업

--- TDGameServer/obj/Debug/net8.0/TDGameServer.AssemblyInfo.cs
+++ TDGameServer/obj/Debug/net8.0/TDGameServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDGameServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e0431de3eba48bde64c33e908dbe8ff549ec3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df0b7d7e1a42d8ff7017a1083f65d35933bd266")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDGameServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDGameServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
